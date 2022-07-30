fecae0efd75193483b615777fd43fe83806f607e
Bernardo Islas <isla_bernard@hotmail.com>
2022-07-30 05:22:39 -0500

    format file countries

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -4,69 +4,53 @@ import { Router } from '@angular/router';
 
 import { CountriesService } from 'src/app/services/countries.service';
 
-
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
+  styleUrls: ['./countries.component.scss'],
 })
 export class CountriesComponent implements OnInit {
+  public listCountries: Country[] = [];
 
-
-   public listCountries: Country[]= [];
-
-   limmit = 8;
-   offset = 0;
+  limmit = 8;
+  offset = 0;
 
   constructor(
     private countriesService: CountriesService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    
     this.getDate();
-          
   }
 
-  public getDate(){
-    this.countriesService.getAllCountries().subscribe(
-      data => {
-       this.listCountries = data;
-       
-      });
+  public getDate() {
+    this.countriesService.getAllCountries().subscribe((data) => {
+      this.listCountries = data;
+    });
   }
 
   nextPage() {
-    console.log("el boton sirve");
+    console.log('el boton sirve');
 
-    this.countriesService.getByPage(this.limmit, this.offset)
-      .subscribe(data => {
-       this.offset += this.limmit;
-        console.log(data)
+    this.countriesService
+      .getByPage(this.limmit, this.offset)
+      .subscribe((data) => {
+        this.offset += this.limmit;
+        console.log(data);
       });
   }
-  
 
-  showCountries(){
+  showCountries() {
     console.log('diste clic');
     console.log(this.listCountries[0]);
-     
-    for(let i=0; i< this.listCountries.length; i++){
-      console.log(this.listCountries[i].name.common );
+
+    for (let i = 0; i < this.listCountries.length; i++) {
+      console.log(this.listCountries[i].name.common);
     }
-    
   }
 
-  showCountriesName(name:string): void {
-    
-      this.router.navigate([`countries/${name}`])
-    
+  showCountriesName(name: string): void {
+    this.router.navigate([`countries/${name}`]);
   }
-
-  
-  
-
 }
-
-
